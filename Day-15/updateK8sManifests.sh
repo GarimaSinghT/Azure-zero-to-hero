c84be064084eb31546f016efec3c7831fdc01730
Garima <garimasinghtomar@gmail.com>
2024-02-19 19:56:07 +0530

    updating with my k8s manifest

--- Day-15/updateK8sManifests.sh
+++ Day-15/updateK8sManifests.sh
@@ -3,7 +3,7 @@
 set -x
 
 # Set the repository URL
-REPO_URL="https://<ACCESS-TOKEN>@dev.azure.com/<AZURE-DEVOPS-ORG-NAME>/voting-app/_git/voting-app"
+REPO_URL="https://2b64xpd3lqj4tga23rwcqh37cgqu7tbtjnopizuepffvatbgdioq@dev.azure.com/viveksingh/voting-app/_git/voting-app"
 
 # Clone the git repository into the /tmp directory
 git clone "$REPO_URL" /tmp/temp_repo
@@ -13,7 +13,7 @@ cd /tmp/temp_repo
 
 # Make changes to the Kubernetes manifest file(s)
 # For example, let's say you want to change the image tag in a deployment.yaml file
-sed -i "s|image:.*|image: <ACR-REGISTRY-NAME>/$2:$3|g" k8s-specifications/$1-deployment.yaml
+sed -i "s|image:.*|image: vvkazurecicd.azurecr.io/$2:$3|g" k8s-specifications/$1-deployment.yaml
 
 # Add the modified files
 git add .
@@ -26,3 +26,4 @@ git push
 
 # Cleanup: remove the temporary directory
 rm -rf /tmp/temp_repo
+
